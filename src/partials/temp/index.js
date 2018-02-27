1aee3ffc11f378d2d5cffe589503ba9f462ddafd
yel <yel@sucsoft.com>
2018-02-27 11:24:00 +0800

    1.0.0

---
+++ src/partials/temp/index.js
@@ -0,0 +1,15 @@
+import './tempController';
+import './temp.scss';
+import temp from './temp.html';
+
+let view = {
+  name :"temp",
+  config:{
+    url:"/temp",
+    template:temp,
+  }
+};
+
+export {
+  view
+}
